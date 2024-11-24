a97a9a8c6b2b14e64d5d0238d0346b482289281c
Antonio Stassi <stassiant@outlook.it>
2024-11-24 23:39:29 +0100

    Testing resources and controllers with example of @Param

---
+++ src/api/api.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('api')
+export class ApiController {
+  @Get('date') // Route GET /api/date
+  getDate(): Date {
+    const currentDate = new Date();
+    console.log(currentDate);
+    return currentDate;
+  }
+
+  @Get('test/:id')
+  getTest(@Param() params: any): string {
+    return `Il parametro è ${params.id}`;
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApiController } from './api/api.controller';
+import { ApiService } from './api/api.service';
+import { DogsModule } from './dogs/dogs.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DogsModule],
+  controllers: [AppController, ApiController],
+  providers: [AppService, ApiService],
 })
 export class AppModule {}
