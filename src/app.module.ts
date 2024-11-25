145a412a405337b2010fe7c4d1a1791bd941ddae
Antonio Stassi <stassiant@outlook.it>
2024-11-25 02:04:43 +0100

    Removed dogs resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ApiController } from './api/api.controller';
 import { ApiService } from './api/api.service';
 import { DogsModule } from './dogs/dogs.module';
+import { DogsModule } from './dogs/dogs.module';
 
 @Module({
   imports: [DogsModule],
