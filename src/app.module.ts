85c5e293fa4aa765ef2c10475f30545fb2b79112
Antonio Stassi <stassiant@outlook.it>
2024-11-25 02:05:38 +0100

    Removed dogs dependency (Module)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiController } from './api/api.controller';
 import { ApiService } from './api/api.service';
-import { DogsModule } from './dogs/dogs.module';
-import { DogsModule } from './dogs/dogs.module';
 
 @Module({
-  imports: [DogsModule],
+  imports: [],
   controllers: [AppController, ApiController],
   providers: [AppService, ApiService],
 })
