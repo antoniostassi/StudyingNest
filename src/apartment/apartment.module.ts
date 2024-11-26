c3df73dfe6b2fe77e2c8209b6c4de57d772b1a37
Antonio Stassi <stassiant@outlook.it>
2024-11-26 23:50:57 +0100

    Importing Apartment TypeOrmModule

--- src/apartment/apartment.module.ts
+++ src/apartment/apartment.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ApartmentController } from './apartment.controller';
 import { ApartmentService } from './apartment.service';
+import { Apartment } from './entity/apartment.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+    imports:[TypeOrmModule.forFeature([Apartment])],
     controllers:[ApartmentController],
     providers:[ApartmentService],
 })
