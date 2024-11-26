3d59eeda1f8907fa2641da9c6cb9e3a5b038bdec
Antonio Stassi <stassiant@outlook.it>
2024-11-26 22:28:51 +0100

    removed controller apartment because it is been using on another module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 // Apartment
-import { ApartmentController } from './apartment/apartment.controller';
 import { ApartmentModule } from './apartment/apartment.module';
 
 @Module({
   imports: [ApartmentModule],
-  controllers: [AppController, ApartmentController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
