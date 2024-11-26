044af0d9e153b03b545f9b08660e03ff04a8dbb4
Antonio Stassi <stassiant@outlook.it>
2024-11-26 23:37:14 +0100

    mysql connection w/ nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,21 @@ import { AppService } from './app.service';
 
 // Apartment
 import { ApartmentModule } from './apartment/apartment.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ApartmentModule],
+  imports: [ApartmentModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'nestjs',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
