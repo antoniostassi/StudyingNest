3cb2a983b3bd9e43ec37b338bfff9d1524c147b5
Antonio Stassi <stassiant@outlook.it>
2024-11-26 21:15:23 +0100

    empty project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ApiController } from './api/api.controller';
-import { ApiService } from './api/api.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, ApiController],
-  providers: [AppService, ApiService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
